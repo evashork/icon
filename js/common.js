61cb888ef89169aeb25ca75d7387551f586e3423
TACO <evashork@naver.com>
2017-06-27 16:52:38 +0900

    프로토타입

---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+	bgColor();
+	fontColor();
+	deviceSel();
+	fontSel();
+	makeIcon();
+});
+
+function bgColor(){
+	$('.btn-bg').click(function(){
+		var hi = $(this).css('background-color');
+	  $('#icon').css({'background-color':hi});
+	})
+}
+
+function fontColor(){
+	$('.btn-cl').click(function(){
+		var hi = $(this).css('color');
+	  $('#icon').css({'color':hi});
+	})
+}
+
+function deviceSel(){
+	$('#radio1').click(function(){
+		$('#icon').css({'border-radius':'20px'});
+	});
+
+	$('#radio2').click(function(){
+		$('#icon').css({'border-radius':'10px'});
+	})
+}
+
+function fontSel(){
+	$('.dropdown-item').click(function(){
+		var hi = $(this).css('font-family');
+	  $('#icon').css({'font-family':hi});
+	})
+}
+
+function iconApple(iconImg){
+	var ilink = document.createElement('link');
+	ilink.setAttribute('rel','apple-touch-icon');
+	ilink.setAttribute('sizes','192x192');
+	ilink.setAttribute('href',iconImg);
+	document.getElementsByTagName('head')[0].appendChild(ilink);
+}
+
+function iconAndor(iconImg){
+	var ilink = document.createElement('link');
+	ilink.setAttribute('rel','icon');
+	ilink.setAttribute('sizes','192x192');
+	ilink.setAttribute('href',iconImg);
+	document.getElementsByTagName('head')[0].appendChild(ilink);
+}
+
+function makeIcon(){
+	$("#btn-save").click(function() { 
+        html2canvas($("#icon"), {
+            onrendered: function(canvas) {
+                theCanvas = canvas;
+                document.body.appendChild(canvas);
+                console.log("hi");
+                var iconURI = canvas.toDataURL("image/png"); 
+                iconApple(iconURI);
+                // Clean up 
+                document.body.removeChild(canvas);
+            }
+        });
+    });
+}
